10edc68476b993c12d36e5426b170bbf40511d77
Sam Hadfield <sthtech@protonmail.com>
2025-08-06 16:03:46 -0500

    Import and export dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
+import { dom } from "./dom.js";
 import { processWeatherData } from "./process-weather-data";
 import { fetchWeatherData } from "./fetch-weather-data";
 
+export { dom };
 export { processWeatherData };
