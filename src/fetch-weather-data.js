29a4ae44dcd3ac0787b2c0a80df82e4422f533f7
Sam Hadfield <sthtech@protonmail.com>
2025-08-06 11:55:21 -0500

    Rename 'api-fetching.js', add async/await
    Rename 'api-fetching-.js' and replace .then() functions with async/await
    and try/catch method

---
+++ src/fetch-weather-data.js
@@ -0,0 +1,24 @@
+export const fetchWeatherData = () => {
+  const submitButton = document.querySelector(
+    "[data-element = 'submit-button']",
+  );
+
+  submitButton.addEventListener("click", () => {
+    const searchBar = document.querySelector("[data-element = 'search-bar']");
+    const currentSearchValue = searchBar.value;
+    queryWeatherAPI(currentSearchValue);
+  });
+
+  async function queryWeatherAPI(currentSearchValue) {
+    try {
+      const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${currentSearchValue}/?key=3B8FBG4CPX75RAUWW5MAZMZCF`,
+      );
+      const data = await response.json();
+      console.log(data.currentConditions);
+    } catch (error) {
+      console.log("You suck, bruh!", error);
+    }
+  }
+};
+fetchWeatherData();
