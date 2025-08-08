cffdfdb4d6f9c0b5a28a54e60f06e0911ea44be2
Sam Hadfield <sthtech@protonmail.com>
2025-08-08 08:33:58 -0500

    Extract 7-day forecast to push to DOM
    Extract 7-day forecast from data obj. The weather API provides a 14-day
    forecast but the spacing in the UI leaves room for 7-days. Extract the
    first 7 days, place them in an array, and add to the extractedDataObj

--- src/process-weather-data.js
+++ src/process-weather-data.js
@@ -1,19 +1,40 @@
 export const processWeatherData = () => {
   const extractRelevantData = (data) => {
-    console.log(data);
-    const mainDataObj = {
+    const daysArr = data.days;
+    const newDaysArr = [];
+
+    for (let i = 0; i < daysArr.length; i++) {
+      if (i > 6) {
+        break;
+      } else {
+        const dayObj = {
+          datetime: daysArr[i].datetime,
+          icon: daysArr[i].icon,
+          tempmax: daysArr[i].tempmax,
+          tempmin: daysArr[i].tempmin,
+        };
+        newDaysArr.push(dayObj);
+      }
+    }
+
+    const extractedDataObj = {
+      address: data.address,
       conditions: data.currentConditions.conditions,
-      description: data.description,
       feelsLike: data.currentConditions.feelslike,
+      forecast: newDaysArr,
       humidity: data.currentConditions.humidity,
-      icon: data.currentConditions.icon,
+      // icon: data.currentConditions.icon,
       precipitation: data.currentConditions.precip,
+      temperature: data.currentConditions.temp,
       windspeed: data.currentConditions.windspeed,
     };
-    console.log(mainDataObj);
+    // console.log(extractedDataObj);
+    return extractedDataObj;
   };
 
   return {
     extractRelevantData,
   };
 };
+
+// For Commit: added 'temperature', removed 'description' (as I already have 'conditions')
