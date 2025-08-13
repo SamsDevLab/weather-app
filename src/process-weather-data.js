ca1a8ccab1d1d470065129ca587560300e1eae01
Sam Hadfield <sthtech@protonmail.com>
2025-08-13 14:07:13 -0500

    Refactor module / include retrieving img filepaths
    Refactor module to better organize the process of extracting data for the
    forecast objects. Also, set up getSvgModule to grab file path of svgs so
    those filepaths can be included in the forecast objects as well as the
    main icon

--- src/process-weather-data.js
+++ src/process-weather-data.js
@@ -1,49 +1,96 @@
-export const processWeatherData = () => {
-  const extractRelevantData = (data) => {
-    const daysArr = data.days;
-    const newDaysArr = [];
-    const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+import { dom } from "./index.js";
+
+export const extractRelevantData = (data) => {
+  console.log(data);
+  const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  const daysArr = data.days;
+  daysArr.splice(7, 8);
+
+  const forecastArr = [];
+  let currentWeatherIcon = "";
+
+  const getDayOfWeek = (date) => {
+    const timestamp = date.datetimeEpoch;
+    const dateFromEpoch = new Date(timestamp * 1000); //
+    const day = dateFromEpoch.getDay();
+    const dayOfWeek = daysOfWeek[day];
+
+    return dayOfWeek;
+  };
+
+  const getTempMax = (date) => {
+    const tempMax = date.tempmax;
+    const roundedTempMax = Math.round(tempMax);
+
+    return roundedTempMax;
+  };
+
+  const getTempMin = (date) => {
+    const tempMin = date.tempmin;
+    const roundedTempMin = Math.round(tempMin);
+
+    return roundedTempMin;
+  };
+
+  async function getSvgModule() {
+    const module = import(`./manifest-svgs`);
+
+    return module;
+  }
+
+  async function buildForecastObjs() {
+    const imgPath = await getSvgModule();
 
     for (let i = 0; i < daysArr.length; i++) {
-      if (i > 6) {
-        break;
-      } else {
-        // Forecast Days
-        const timestamp = daysArr[i].datetimeEpoch;
-        const dateFromEpoch = new Date(timestamp * 1000); //
-        const day = dateFromEpoch.getDay();
+      const dayOfWeek = getDayOfWeek(daysArr[i]);
+      const forecastIcon = await imgPath.getSvg(daysArr[i].icon);
+      const tempmax = getTempMax(daysArr[i]);
+      const tempmin = getTempMin(daysArr[i]);
 
-        // Icons
+      const dayObj = {
+        dayOfWeek,
+        forecastIcon,
+        tempmax,
+        tempmin,
+      };
 
-        const dayObj = {
-          dayOfWeek: daysOfWeek[day],
-          icon: daysArr[i].icon,
-          tempmax: daysArr[i].tempmax,
-          tempmin: daysArr[i].tempmin,
-        };
-        // console.log(dayObj);
-        newDaysArr.push(dayObj);
-      }
+      forecastArr.push(dayObj);
     }
+  }
 
-    const extractedDataObj = {
+  async function getIconImage() {
+    const imgPath = await getSvgModule();
+    currentWeatherIcon = await imgPath.getSvg(data.currentConditions.icon);
+  }
+
+  const getDataObj = () => {
+    const dataObj = {
       address: data.address,
       conditions: data.currentConditions.conditions,
       feelsLike: data.currentConditions.feelslike,
-      forecast: newDaysArr,
+      forecast: forecastArr,
       humidity: data.currentConditions.humidity,
-      icon: data.currentConditions.icon,
+      icon: currentWeatherIcon,
       precipitation: data.currentConditions.precip,
       temperature: data.currentConditions.temp,
       windspeed: data.currentConditions.windspeed,
     };
-    // console.log(extractedDataObj);
-    return extractedDataObj;
+
+    return dataObj;
   };
 
-  return {
-    extractRelevantData,
-  };
+  async function extractDataObj() {
+    try {
+      await buildForecastObjs();
+      await getIconImage();
+      const dataObj = getDataObj();
+      dom(dataObj);
+    } catch (error) {
+      console.log("Cannot complete dataObj", error);
+    }
+  }
+
+  extractDataObj();
 };
 
 // For Commit: added 'temperature', removed 'description' (as I already have 'conditions')
